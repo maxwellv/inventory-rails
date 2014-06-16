1a3f639ac9c2388ffca65f46fb281ca105466de4
maxwellv <max.vance@gmail.com>
2014-06-16 10:21:23 -0500

    added Devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :rememberable
+
+  has_many :jewelries, through: :ownerships
+
+  validates :username, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "username can only contain letters and numbers"}
+  validates :email, uniqueness: true
 end
